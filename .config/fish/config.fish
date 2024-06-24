cdb55250a41435d0508035dca0ae38718de0d512
JmPotato <ghzpotato@gmail.com>
2024-06-24 15:53:44 +0800

    Update the config files
    Signed-off-by: JmPotato <ghzpotato@gmail.com>

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,23 @@
-if type -q exa
-    alias ll "exa -l -g --icons"
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Setup
+ulimit -n 82920
+eval "$(/opt/homebrew/bin/brew shellenv)"
+starship init fish | source
+thefuck --alias | source
+
+# Alias
+if type -q lsd
+    alias ll "lsd -l -g"
     alias lla "ll -a"
 end
 
 if type -q git
     alias g git
+end
+
+if type -q brew
+    alias brewx "brew update && brew upgrade --greedy ; brew cleanup --prune=all"
 end
